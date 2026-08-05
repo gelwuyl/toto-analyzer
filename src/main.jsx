806479904c5f0c0eaa1ed037d3b1c22310fc421f
gelwuyl <gelwuyl@icloud.com>
2026-08-05 16:28:27 +0800

    add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import TotoAnalyzer from './TotoAnalyzer.jsx'
+
+createRoot(document.getElementById('root')).render(<TotoAnalyzer />)
