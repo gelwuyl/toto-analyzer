6930cf658820dab6d486dc05affeeba65422ab8a
gelwuyl <gelwuyl@icloud.com>
2026-08-05 16:46:33 +0800

    style: add Tailwind build + cron Mon/Thu 10pm SGT (src/main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import TotoAnalyzer from './TotoAnalyzer.jsx'
 
 createRoot(document.getElementById('root')).render(<TotoAnalyzer />)
